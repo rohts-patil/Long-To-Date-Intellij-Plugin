2e3ecb00b36595220bcda931df299ba2ccf1f2fa
Rohit Patil <rohts.patil@gmail.com>
2020-01-22 17:38:33 +0530

    Update LongToDateAction.java

--- src/com/tpt/intellij/LongToDateAction.java
+++ src/com/tpt/intellij/LongToDateAction.java
@@ -7,19 +7,36 @@ import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/*
+ * @author rohit.patil
+ */
 public class LongToDateAction extends AnAction {
-  /**
-   * Convert selected text to a URL friendly string.
-   *
-   * @param event
-   */
+
   @Override
   public void actionPerformed(AnActionEvent event) {
     final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
     CaretModel caretModel = editor.getCaretModel();
 
+    String dataUTC =
+        MillisToDate.convert(
+            Long.parseLong(caretModel.getCurrentCaret().getSelectedText()),
+            TimeZone.getTimeZone("UTC"));
+
+    String dataLocal =
+        MillisToDate.convert(
+            Long.parseLong(caretModel.getCurrentCaret().getSelectedText()),
+            Calendar.getInstance().getTimeZone());
+
     Messages.showMessageDialog(
-        MillisToDate.convert(Long.parseLong(caretModel.getCurrentCaret().getSelectedText())),
+        "UTC:-\n"
+            + dataUTC
+            + "\n"
+            + Calendar.getInstance().getTimeZone().getID()
+            + ":-\n"
+            + dataLocal,
         "Long to Date",
         Messages.getInformationIcon());
   }
